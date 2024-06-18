5fc9f892f9bb40b335ba9cd4ad6daa5cc7ba0229
Abhishek-coder2001 <abhishek800.roy@gmail.com>
2024-06-18 17:18:39 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const router = require("./router/auth-router");
+const connectDatabase = require("./database/db");
+const adminRoute = require("./router/admin-router");
+const errorMiddleware = require("./middleware/error.middleware");
+
+// USE MIDDLEWARE
+
+var corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET,POST,PUT,DELETE,PATCH",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.use(express.json()); //to convert all thedata into json format
+app.use("/api/v1", router); //define a path
+
+app.use("/api/admin", adminRoute);
+
+//  Call the error middleware
+app.use(errorMiddleware);
+
+/********************************************** */
+
+const PORT = process.env.PORT || 5000;
+
+connectDatabase()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server run at: http://localhost:${PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("Error in database & server");
+  });
